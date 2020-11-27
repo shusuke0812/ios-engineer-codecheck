79ef298107af252dffcaab32a964ded19f8eaf7a
shusuke0812 <shu.ota0812@gmail.com>
2020-11-27 15:44:12 +0900

    [add]ViewとViewModelファイルを追加

---
+++ iOSEngineerCodeCheck/View/BaseView/GitHubRepositoryDetailBaseView.swift
@@ -0,0 +1,13 @@
+//
+//  GitHubRepositoryDetailBaseView.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Shusuke Ota on 2020/11/27.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class GitHubRepositoryDetailBaseView: UIView {
+    
+}
---
+++ iOSEngineerCodeCheck/ViewModel/GitHubRepositorySearchViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  GitHubRepositorySearchViewModel.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Shusuke Ota on 2020/11/27.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+protocol GitHubRepositorySearchViewModelDelegate: class {
+}
+
+class GitHubRepositorySearchViewMoodel {
+}
+
