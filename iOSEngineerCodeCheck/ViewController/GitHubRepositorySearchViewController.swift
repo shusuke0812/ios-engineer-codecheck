4e8f3da4ce0a039333275e89c6de4976eb4a2656
shusuke0812 <shu.ota0812@gmail.com>
2020-11-28 17:30:46 +0900

    [remove]不要なコメントを削除

--- iOSEngineerCodeCheck/ViewController/GitHubRepositorySearchViewController.swift
+++ iOSEngineerCodeCheck/ViewController/GitHubRepositorySearchViewController.swift
@@ -22,7 +22,6 @@ class GitHubRepositorySearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         self.viewModel = GitHubRepositorySearchViewModel(gitHubRepositorySearchRepository: GitHubRepositorySearchRepository())
         self.setDelegateDataSource()
     }
