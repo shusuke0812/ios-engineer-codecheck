96ca55332352437ad78a08a58666a74ef6e66c69
shusuke0812 <shu.ota0812@gmail.com>
2020-11-28 11:14:15 +0900

    [add]ネットワークエラー種別の列挙型を追加

--- iOSEngineerCodeCheck/Static/Common.swift
+++ iOSEngineerCodeCheck/Static/Common.swift
@@ -8,10 +8,29 @@
 
 import Foundation
 
+/// モジュール内共通のデータ
 struct Common {
     /// API URLのプレフィックス
     struct ApiUrl {
         /// GET /search/repositories
         internal static let gitHubSearchApi = "https://api.github.com/search/repositories"
     }
+}
+/// ネットワークエラー
+enum NetworkError: Error {
+    case invalidUrl         // 不正なURL
+    case invalidResponse    // 不正なレスポンス
+    case unknown            // 想定外エラー
+    func description() -> String {
+        switch self {
+            case .invalidUrl:       return "DEBUG： 不正なURLです"
+            case .invalidResponse:  return "DEBUG： 不正なレスポンスです"
+            case .unknown:          return "DEBUG： レスポンスに失敗しました"
+        }
+    }
+}
+/// クライアントエラー
+enum ClientError: Error {
+    case parseField // パースエラー
+    case unknown    // 想定外エラー
 }
