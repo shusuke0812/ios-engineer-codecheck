c7dc4e9f95fece4ac44b3e2185e883860296b15a
shusuke0812 <shu.ota0812@gmail.com>
2020-11-28 11:04:39 +0900

    [add]GitHubRepositorySearchAPIのプレフィックス構造体を追加

---
+++ iOSEngineerCodeCheck/Static/Common.swift
@@ -0,0 +1,16 @@
+//
+//  Common.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Shusuke Ota on 2020/11/28.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Common {
+    struct ApiUrl {
+        /// GitHubReppositoryのSearch URLプレフィックス
+        internal static let gitHubSearchApi = "https://api.github.com/search/repositories"
+    }
+}
