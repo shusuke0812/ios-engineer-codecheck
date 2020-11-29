562ab1693b8465d4995141c5026f521d41a59f4e
shusuke0812 <shu.ota0812@gmail.com>
2020-11-29 09:16:33 +0900

    [fix]ViewControllerからView部分を分離

--- iOSEngineerCodeCheck/View/BaseView/GitHubRepositoryDetailBaseView.swift
+++ iOSEngineerCodeCheck/View/BaseView/GitHubRepositoryDetailBaseView.swift
@@ -10,4 +10,30 @@ import UIKit
 
 class GitHubRepositoryDetailBaseView: UIView {
     
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var starNumberLabel: UILabel!
+    @IBOutlet weak var watchNumberLabel: UILabel!
+    @IBOutlet weak var forkNumberLabel: UILabel!
+    @IBOutlet weak var issueNumberLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.initUI()
+    }
+}
+// MARK: - Setting UI Method
+extension GitHubRepositoryDetailBaseView {
+    private func initUI() {
+        let defauletNumber = 0
+        self.avatarImageView.image = UIImage(named: "avattar_default")
+        self.titleLabel.text = ""
+        self.starNumberLabel.text = "\(defauletNumber)"
+        self.watchNumberLabel.text = "\(defauletNumber)"
+        self.forkNumberLabel.text = "\(defauletNumber)"
+        self.issueNumberLabel.text = "\(defauletNumber)"
+    }
+    func setUI() {
+    }
 }
