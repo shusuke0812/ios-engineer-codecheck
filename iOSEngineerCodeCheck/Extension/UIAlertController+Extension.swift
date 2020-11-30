9e0edf45573cf46f4787684ea6f629026aad9574
shusuke0812 <shu.ota0812@gmail.com>
2020-11-30 09:45:35 +0900

    [add]Alert表示メソッドをextensionに追加

---
+++ iOSEngineerCodeCheck/Extension/UIAlertController+Extension.swift
@@ -0,0 +1,35 @@
+//
+//  UIAlertController+Extension.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Shusuke Ota on 2020/11/30.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+extension UIAlertController {
+    /// アラート表示
+    /// - Parameters:
+    ///   - style: アラート種類
+    ///   - viewController: アラートを表示させるViewController
+    ///   - title: タイトル
+    ///   - message: サブタイトル
+    ///   - okButtonTitle: OKボタンのタイトル
+    ///   - cancelButtonTitle: キャンセルボタンタイトル
+    internal static func showAlert(style: UIAlertController.Style, viewController: UIViewController,
+                                   title: String?, message: String?, okButtonTitle: String?, cancelButtonTitle: String?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        let okAction = UIAlertAction(title: okButtonTitle, style: .default, handler: { (action: UIAlertAction!) -> Void in
+            print("DEBUG: OKボタンが押されました")
+        })
+        alert.addAction(okAction)
+        if let cancelButtonTitle = cancelButtonTitle {
+            let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel, handler: { (action: UIAlertAction!) -> Void in
+                print("DEEBUG: キャンセルボタンが押されました")
+            })
+            alert.addAction(cancelAction)
+        }
+        viewController.present(alert, animated: true, completion: nil)
+    }
+}
