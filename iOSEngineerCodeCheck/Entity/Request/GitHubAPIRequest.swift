94d5f1b8708be4f4e8888e45d19826f17b3b52f8
shusuke0812 <shu.ota0812@gmail.com>
2021-01-23 14:09:56 +0900

    [add/fix]APIのリクエスト型を定義するファイルを追加し、リクエスト・レスポンスでディレクトリを分けた

---
+++ iOSEngineerCodeCheck/Entity/Request/GitHubAPIRequest.swift
@@ -0,0 +1,20 @@
+//
+//  GitHubAPIRequest.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Shusuke Ota on 2021/1/23.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol GitHubAPIRequest {
+    var baseURL: String { get }
+    var path: String { get }
+}
+// MARK: - Custom Setting
+extension GitHubAPIRequest {
+    var baseURL: String {
+        "https://api.github.com"
+    }
+}
