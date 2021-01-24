49956428887a13a75e6171435239ce951f95c284
shusuke0812 <shu.ota0812@gmail.com>
2021-01-24 16:13:03 +0900

    [fix]リポジトリ検索一覧のセルUIを変更
    ・スター数、新しい情報、タイトル・サブタイトルが詳細を見る時の判断材料だと思われるので記載
    ・WebのGitHubを参考にした

--- iOSEngineerCodeCheck/View/TableViewCell/GitHubRepositoryCell.swift
+++ iOSEngineerCodeCheck/View/TableViewCell/GitHubRepositoryCell.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class GitHubRepositoryCell: UITableViewCell {
-    @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var language: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var starLabel: UILabel!
+    @IBOutlet weak var starImageView: UIImageView!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var languageView: UIView!
+    @IBOutlet weak var updatedDateLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.initUI()
     }
 }
 // MARK: - Setting UI Method
 extension GitHubRepositoryCell {
+    private func initUI() {
+        // 文字色の設定
+        self.starLabel.textColor = .lightGray
+        self.languageLabel.textColor = .lightGray
+        self.updatedDateLabel.textColor = .lightGray
+        // スター色の設定
+        self.starImageView.tintColor = .gray
+        // 言語アイコンの体裁設定
+        self.languageView.backgroundColor = .gray
+        self.languageView.clipsToBounds = true
+        self.languageView.layer.cornerRadius = self.languageView.frame.width / 2
+    }
     func setUI(gitHubRepository: GitHubRepository.Item) {
-        self.title.text = gitHubRepository.name
-        self.language.text = gitHubRepository.language
+        self.titleLabel.text = gitHubRepository.name
+        self.languageLabel.text = gitHubRepository.language
     }
 }
