1407b0294164b6aeb660ba507c6df0f79e47da72
shusuke0812 <shu.ota0812@gmail.com>
2021-01-25 16:52:33 +0900

    [add]SafariViewを開くためのメソッドをUIViewControllerのextensionに追加

--- iOSEngineerCodeCheck/Extension/UIViewController+Extension.swift
+++ iOSEngineerCodeCheck/Extension/UIViewController+Extension.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SafariServices
 
 extension UIViewController {
     /// キーボード以外の画面をタップした時にキーボードを閉じる処理
@@ -17,5 +18,21 @@ extension UIViewController {
     }
     @objc private func dissmissKeyboard() {
         self.view.endEditing(true)
+    }
+    /// Safariで指定したURLのページを開く
+    /// - Parameter urlString: URL文字列
+    internal func transitionSafariViewController(urlString: String) {
+        let url: URL?
+        // 不正なURLであるかを判定する（不正なものはhttpsスキームをつけてブラウザでエラーハンドリングする）
+        if urlString.hasPrefix("http://") || urlString.hasPrefix("https://") {
+            url = URL(string: urlString)
+        } else {
+            url = URL(string: "https://" + urlString)
+        }
+        // Safariでwebページを開く
+        if let url = url {
+            let safariVC: SFSafariViewController = SFSafariViewController(url: url)
+            self.present(safariVC, animated: true, completion: nil)
+        }
     }
 }
