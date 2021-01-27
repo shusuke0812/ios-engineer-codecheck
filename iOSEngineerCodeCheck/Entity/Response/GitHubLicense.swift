18e2623bf6b8e4fe66b6fd60e2898bd961b6321d
shusuke0812 <shu.ota0812@gmail.com>
2021-01-27 15:56:11 +0900

    [add]GitHubのLICENSEデータを受け取る構造体を追加

---
+++ iOSEngineerCodeCheck/Entity/Response/GitHubLicense.swift
@@ -0,0 +1,26 @@
+//
+//  GitHubLicense.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Shusuke Ota on 2021/1/27.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+/// GitHubのライセンスデータ格納用
+class GitHubLicense: Codable {
+    /// 説明
+    let description: String
+    /// 文面
+    let body: String
+    
+    enum CodingKeys: String, CodingKey {
+        case description
+        case body
+    }
+    init(description: String, body: String) {
+        self.description = description
+        self.body = body
+    }
+}
