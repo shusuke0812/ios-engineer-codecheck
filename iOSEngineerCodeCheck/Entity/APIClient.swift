7981724cf1f677df890b773419ce33da1d30668d
shusuke0812 <shu.ota0812@gmail.com>
2021-02-11 17:01:32 +0900

    [add]リポジトリクラスに記述した`API呼び出し処理`をAPIClient型に集約

--- iOSEngineerCodeCheck/Entity/APIClient.swift
+++ iOSEngineerCodeCheck/Entity/APIClient.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+protocol APIClientProtocol {
+    /// GitHubAPIをコールするメソッド
+    /// - Parameters:
+    ///   - request: APIコール用のリクエスト
+    ///   - completion: APIレスポンス取得の成功、失敗ハンドル
+    func sendRequest<T: GitHubAPIRequest>(_ request: T, completion: @escaping (Result<T.Response, APIClientError>) -> Void)
+}
+class APIClient: APIClientProtocol {
+}
+// MARK: - API Base Method
+extension APIClient {
+    func sendRequest<T: GitHubAPIRequest>(_ request: T, completion: @escaping (Result<T.Response, APIClientError>) -> Void) {
+        let session = URLSession.shared
+        let task = session.dataTask(with: request.buildURLRequest()) { data, response, error in
+            if let error = error {
+                completion(.failure(.connectionError(error)))
+                return
+            }
+            guard let data = data, let response = response as? HTTPURLResponse else {
+                completion(.failure(.unknown))
+                return
+            }
+            print("DEBUG: status=\(response.statusCode)\n", response)
+            let decoder = JSONDecoder()
+            // TODO: dateのフォーマット型はレスポンスによるのでデコーダー処理を別に定義した方が良さそう（ex. APIRequest型に実装する）
+            decoder.dateDecodingStrategy = .iso8601
+            if (200..<300).contains(response.statusCode) {
+                do {
+                    let apiResponse = try decoder.decode(T.Response.self, from: data)
+                    completion(.success(apiResponse))
+                } catch {
+                    completion(.failure(.responseParseError(error)))
+                }
+            } else {
+                do {
+                    // TODO: 他のAPIError型でも使えるように汎用化する
+                    let apiError = try decoder.decode(GitHubAPIError.self, from: data)
+                    completion(.failure(.apiError(apiError)))
+                } catch {
+                    completion(.failure(.responseParseError(error)))
+                }
+            }
+        }
+        task.resume()
+    }
+}
