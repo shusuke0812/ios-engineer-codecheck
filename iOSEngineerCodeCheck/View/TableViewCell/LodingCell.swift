54ec039a88c25aeab94643e249ba3b84be854638
shusuke0812 <shu.ota0812@gmail.com>
2021-02-15 14:36:45 +0900

    [add]TableViewのフッターに追加するAPIロード用のセルを追加

---
+++ iOSEngineerCodeCheck/View/TableViewCell/LodingCell.swift
@@ -0,0 +1,24 @@
+//
+//  LodingCell.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Shusuke Ota on 2021/2/15.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class LodingCell: UITableViewCell {
+
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+// MARK: - Animation Method
+extension LodingCell {
+    func startAnimation() {
+        self.activityIndicatorView.startAnimating()
+    }
+}
