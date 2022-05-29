c47ea63643f4d05db621a61d1db5586c6e48571c
shusuke0812 <shu.ota0812@gmail.com>
2022-05-30 01:44:53 +0900

    remove: 不要なコメント、ドキュメントを削除

--- iOSEngineerCodeCheck/ScreenModule/Search/GitHubRepositorySearchViewModel.swift
+++ iOSEngineerCodeCheck/ScreenModule/Search/GitHubRepositorySearchViewModel.swift
@@ -17,30 +17,25 @@ protocol GitHubRepositorySearchViewModelDelegate: AnyObject {
 }
 
 class GitHubRepositorySearchViewModel: NSObject {
-    /// GitHubRepositoryのリポジトリクラス
     private let gitHubRepositorySearchRepository: GitHubRepositorySearchRepositoryProtocol
-    /// GitHubのリポジトリ一覧
     var gitHubRepositorys: [GitHubRepository] = []
-    /// GitHubRepositorySearchViewModelのデリゲート
+
     weak var delegate: GitHubRepositorySearchViewModelDelegate?
-    /// GitHubリポジトリの検索結果最大取得数
+
     let maxGitHubRepositorySearchCount = 20
-    /// GitHubリポジトリの検索ページ番号
     var gitHubRepositorySearchPageNumber: Int = 1
-    /// APIの読み込み状況
     var apiLoadStatus: APILoadingStatus = .initial
-    /// 検索ワード
     var searchWord: String = ""
 
     init(gitHubRepositorySearchRepository: GitHubRepositorySearchRepositoryProtocol) {
         self.gitHubRepositorySearchRepository = gitHubRepositorySearchRepository
     }
 }
+
 // MARK: - API Method
+
 extension GitHubRepositorySearchViewModel {
-    /// GitHubRepositoryを取得する
     func getGitHubRepositorys() {
-        /// API読み込み状況
         if self.apiLoadStatus == .fetching || self.apiLoadStatus == .full {
             return
         } else {
@@ -62,14 +57,15 @@ extension GitHubRepositorySearchViewModel {
             }
         }
     }
-    /// APIのコールパラメータを初期化する
     func initAPIParameters() {
         self.gitHubRepositorySearchPageNumber = 1
         self.apiLoadStatus = .initial
         self.gitHubRepositorys = []
     }
 }
+
 // MARK: - UITableView DataSource Method
+
 extension GitHubRepositorySearchViewModel: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         self.gitHubRepositorys.count
