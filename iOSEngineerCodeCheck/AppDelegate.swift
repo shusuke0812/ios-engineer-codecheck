40b917f944bbab4a4795ae8d2809aba1644838b3
shusuke0812 <shu.ota0812@gmail.com>
2025-02-01 17:26:29 +0900

    add: Logger

--- iOSEngineerCodeCheck/AppDelegate.swift
+++ iOSEngineerCodeCheck/AppDelegate.swift
@@ -14,19 +14,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("--- アプリ起動 ---")
-        
+        kLogger.debugPrint("アプリ起動")
+
         NetworkConnectivity.shared.setup()
-        print("DEBUG: isOnline=\(NetworkConnectivity.shared.isOnline)")
+        kLogger.debugPrint("isOnline=\(NetworkConnectivity.shared.isOnline)")
         return true
     }
 
     // MARK: - UISceneSession Lifecycle
-    
+
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        print("--- Scene呼び出し ---")
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
---
+++ iOSEngineerCodeCheck/Utility/Logger.swift
@@ -0,0 +1,9 @@
+//
+//  Logger.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Shusuke Ota on 2025/2/1.
+//  Copyright © 2025 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
