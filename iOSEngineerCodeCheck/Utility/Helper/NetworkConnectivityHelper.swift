2361473cbc6bc60ae7906a17cf7c5a933260d9f9
shusuke0812 <shu.ota0812@gmail.com>
2025-02-02 17:38:33 +0900

    fix: NetworkConnectivity改良

--- iOSEngineerCodeCheck/Utility/Helper/NetworkConnectivityHelper.swift
+++ iOSEngineerCodeCheck/Utility/Helper/NetworkConnectivityHelper.swift
@@ -11,18 +11,42 @@ import Network
 
 final class NetworkConnectivity {
     static let shared = NetworkConnectivity()
-    
+
     private let monitor: NWPathMonitor
-    
+    private var isFirstSetup = true
+
     var isOnline: Bool {
         monitor.currentPath.status == .satisfied
     }
-    
+
     private init() {
-        self.monitor = NWPathMonitor()
+        monitor = NWPathMonitor()
     }
-    
+
+    deinit {
+        monitor.cancel()
+    }
+
     func setup() {
-        monitor.start(queue: .global(qos: .background))
+        kLogger.debugPrint()
+
+        let semaphore = DispatchSemaphore(value: 0)
+        monitor.pathUpdateHandler = { [weak self] path in
+            guard let self else {
+                return
+            }
+            if self.isFirstSetup {
+                kLogger.debugPrint("update network status=\(path.status)")
+                self.isFirstSetup = false
+                semaphore.signal()
+            }
+        }
+        monitor.start(queue: .global())
+        switch semaphore.wait(timeout: .now() + 1) {
+        case .success:
+            kLogger.debugPrint("success")
+        case .timedOut:
+            isFirstSetup = false
+        }
     }
 }
