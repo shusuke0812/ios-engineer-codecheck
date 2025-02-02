dd4ec280a0ffd9c1bf2e2f2fc629518389fd1685
shusuke0812 <shu.ota0812@gmail.com>
2025-02-02 15:05:53 +0900

    add: Logger

--- iOSEngineerCodeCheck/Utility/Logger.swift
+++ iOSEngineerCodeCheck/Utility/Logger.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+let kLogger = Logger()
+
+class Logger {
+    private let dateFormatter: DateFormatter
+    private var timeStamp: String {
+        dateFormatter.string(from: Date())
+    }
+
+    init() {
+        dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss.SSS"
+        dateFormatter.locale = Locale(identifier: "ja_JP")
+    }
+
+    func debugPrint(filePath: String = #file, function: String = #function, line: Int = #line, _ message: String = "") {
+        basePrint(filePath: filePath, function: function, line: line, message)
+    }
+
+    private func basePrint(filePath: String, function: String, line: Int, _ message: String) {
+        #if DEBUG
+        print("debug: \(className(from: filePath)) \(function) \(line) \(message)")
+        #endif
+    }
+
+    private func className(from filepath: String) -> String {
+        let fileName = filepath.components(separatedBy: "/").last
+        return fileName?.components(separatedBy: ".").first ?? ""
+    }
+}
