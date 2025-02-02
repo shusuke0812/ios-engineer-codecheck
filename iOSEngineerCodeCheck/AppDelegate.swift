e4306b371628730a70e22650503f8ae5affbf54e
shusuke0812 <shu.ota0812@gmail.com>
2025-02-02 18:45:02 +0900

    fix: デバッグ用のprint文をkLoggerに置き換え

--- iOSEngineerCodeCheck/AppDelegate.swift
+++ iOSEngineerCodeCheck/AppDelegate.swift
@@ -34,7 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-        print("--- アプリ停止 ---")
+        kLogger.debugPrint("--- アプリ停止 ---")
     }
     */
 }
