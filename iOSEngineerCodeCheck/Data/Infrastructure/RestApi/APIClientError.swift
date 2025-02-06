c10c6bee187256b345c31d545e065606faca62c2
shusuke0812 <shu.ota0812@gmail.com>
2025-02-06 22:51:35 +0900

    fix: APIErrorのdescriptionをメソッドからプロパティに変更

--- iOSEngineerCodeCheck/Data/Infrastructure/RestApi/APIClientError.swift
+++ iOSEngineerCodeCheck/Data/Infrastructure/RestApi/APIClientError.swift
@@ -19,7 +19,7 @@ enum APIClientError: Error {
     /// 不明なエラー
     case unknown
 
-    func description() -> String {
+    var description: String {
         switch self {
         case .connectionError:      return "通信に失敗"
         case .responseParseError:   return "レスポンスの変換に失敗"
